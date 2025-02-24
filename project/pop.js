92eee31298377dc411a1f501243d58be5cbf9591
Tushar Joshi <159403768+TJoshi3637@users.noreply.github.com>
2025-02-24 17:06:22 +0530

    Add files via upload

---
+++ project/pop.js
@@ -0,0 +1,7 @@
+document.getElementById('open-signin-popup').addEventListener('click', function() {
+    document.getElementById('signin-popup').style.display = 'block';
+});
+
+document.getElementById('close-signin-popup').addEventListener('click', function() {
+    document.getElementById('signin-popup').style.display = 'none';
+});
